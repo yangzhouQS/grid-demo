977055935d85b49cdaff7dc9efeae7e90d7e3f95
yangzhou <263849722@qq.com>
2022-10-23 00:00:11 +0800

    babel

--- src/ts-types/column/action.ts
+++ src/ts-types/column/action.ts
@@ -6,71 +6,75 @@ import { MaybePromise } from "../base";
 export type RecordBoolean<T = any> = boolean | ((record: T) => boolean);
 
 export interface BaseActionOption {
-  disabled?: RecordBoolean;
+	disabled?: RecordBoolean;
 }
 
 export type ActionListener = (
-  record: any,
-  cell: CellAddress & {
-    grid: ListGridAPI<any>;
-  }
+	record: any,
+	cell: CellAddress & {
+		grid: ListGridAPI<any>;
+	}
 ) => void;
+
 export interface ActionOption extends BaseActionOption {
-  action?: ActionListener;
+	action?: ActionListener;
 }
+
 export interface EditorOption extends BaseActionOption {
-  readOnly?: RecordBoolean;
+	readOnly?: RecordBoolean;
 }
+
 export type ButtonActionOption = ActionOption;
 
 export interface InlineMenuEditorOption<T> extends EditorOption {
-  classList?: string | string[];
-  options?:
-    | ColumnMenuItemOptions
-    | ((record: T | undefined) => ColumnMenuItemOptions);
+	classList?: string | string[];
+	options?:
+		| ColumnMenuItemOptions
+		| ((record: T | undefined) => ColumnMenuItemOptions);
 }
 
 export interface InlineInputEditorOption extends EditorOption {
-  classList?: string | string[];
-  type?: string;
+	classList?: string | string[];
+	type?: string;
 }
 
 type GetValueResult<T, R> = (
-  value: string,
-  info: { grid: ListGridAPI<T>; col: number; row: number }
+	value: string,
+	info: { grid: ListGridAPI<T>; col: number; row: number }
 ) => R;
+
 export interface SmallDialogInputEditorOption<T> extends EditorOption {
-  classList?: string | string[];
-  type?: string;
-  helperText?: string | GetValueResult<T, string>;
-  inputValidator?: GetValueResult<T, MaybePromise<string>>;
-  validator?: GetValueResult<T, MaybePromise<string>>;
+	classList?: string | string[];
+	type?: string;
+	helperText?: string | GetValueResult<T, string>;
+	inputValidator?: GetValueResult<T, MaybePromise<string>>;
+	validator?: GetValueResult<T, MaybePromise<string>>;
 }
 
 export type GetRadioEditorGroup<T> = (target: {
-  grid: ListGridAPI<T>;
-  col: number;
-  row: number;
+	grid: ListGridAPI<T>;
+	col: number;
+	row: number;
 }) => CellAddress[];
 
 export interface RadioEditorOption<T> extends EditorOption {
-  /** @deprecated Use checkAction instead. */
-  group?: GetRadioEditorGroup<T>;
-  checkAction?: ActionListener;
+	/** @deprecated Use checkAction instead. */
+	group?: GetRadioEditorGroup<T>;
+	checkAction?: ActionListener;
 }
 
 export type SortOption<T> =
-  | boolean
-  | (string & keyof T)
-  | ((arg: {
-      order: "asc" | "desc";
-      col: number;
-      row: number;
-      grid: ListGridAPI<T>;
-    }) => void);
+	| boolean
+	| (string & keyof T)
+	| ((arg: {
+	order: "asc" | "desc";
+	col: number;
+	row: number;
+	grid: ListGridAPI<T>;
+}) => void);
 
 export interface SortHeaderActionOption<T> extends BaseActionOption {
-  sort?: SortOption<T>;
+	sort?: SortOption<T>;
 }
 
 export type ColumnActionOption = "CHECK" | "check" | "INPUT" | "input";
